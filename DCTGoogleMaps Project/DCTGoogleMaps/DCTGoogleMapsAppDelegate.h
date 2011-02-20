fb29432529704cc7ffb4e417a92d731e8399be8d
Daniel Tull <dt@danieltull.co.uk>
2011-02-20 23:18:20 +0000

    Initial commit

---
+++ DCTGoogleMaps Project/DCTGoogleMaps/DCTGoogleMapsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DCTGoogleMapsAppDelegate.h
+//  DCTGoogleMaps
+//
+//  Created by Daniel Tull on 20.02.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTGoogleMapsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
